ae04b8e4547e2df724ec912d1d8464bdf4284fca
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-06 17:40:26 +0900

    fix validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # validates :nickname, :email, :password, :firstname,:lastname, :birthday, presence: true
+  validates :nickname, :email, :password, :firstname,:lastname, :birthday, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :address
