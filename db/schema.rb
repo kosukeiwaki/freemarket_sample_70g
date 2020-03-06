6c8c02b7339dc0b2e09c0c90981e769d546c4faa
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-06 11:19:30 +0900

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_04_084645) do
+
   
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
