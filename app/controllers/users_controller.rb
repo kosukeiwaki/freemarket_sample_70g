8bf01dcef634be2754ad24bfa743dab51b06e5a0
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-06 17:35:34 +0900

    add fields_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
 
-  # def create(user_params)
+  def index
+
+  end
+
+  # def new
+  #   @user = User.new
+  #   @user.build_address
   # end
 
-  # private
-  #   def user_params
-  #     params.require(:user).permit(:)
-  #   end
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname,:email,:firstname,:lastname,:birthday,:lastname_kana,:firstname_kana,
+                                  address_attributes:[:id, :zip, :prefecture, :city, :block, :building, :phone_number,
+                                    :lastname, :firstname, :lastname_kana, :firstname_kana])
+  end
 
 end
