a499be9f0924ae4868b32f43d9d8fdbc8feb23a3
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-07 22:56:44 +0900

    add regex

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :zip, :prefecture, :city, :lastname, :firstname, :lastname_kana, :firstname_kana ,presence: true
+  validates :zip, :prefecture, :city, :lastname, :firstname, :lastname_kana, :firstname_kana, presence: true
+  validates :zip, numericality: true
+  validates :lastname, :firstname, format: {
+    with: /\A[ぁ-んァ-ヶー一-龠]+\z/, message: "全角で入力してください"
+  }
+  validates :firstname_kana, :lastname_kana, format: {
+    with: /\A[ぁ-んー－]+\z/, message: "全角ひらがなで入力してください"
+  }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :nickname, :email, :password, :firstname,:lastname, :birthday, presence: true
+  validates :nickname, :email, :password, :firstname,:lastname, :lastname_kana, :firstname_kana, :birthday, presence: true
+  validates :email, format: {
+    with:  /\A\S+@\S+\.\S+\z/, message: "メールアドレスが正しくありません"
+  }
+  validates :lastname, :firstname, format: {
+    with: /\A[ぁ-んァ-ヶー一-龠]+\z/, message: "全角で入力してください"
+  }
+  validates :lastname_kana, :firstname_kana, format: {
+    with: /\A[ぁ-んー－]+\z/, message: "全角ひらがなで入力してください"
+  }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :address
   accepts_nested_attributes_for :address
-  
+
 end
