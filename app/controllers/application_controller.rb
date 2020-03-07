5268ea68390a543e456ed684e8dce58b2d8a1fab
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-07 17:53:19 +0900

    add signup function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:birthday,:lastname_kana,:firstname_kana])
+    devise_parameter_sanitizer.permit(:sign_up,
+       keys: [:nickname,:firstname,:lastname,:birthday,:lastname_kana,:firstname_kana])
   end
 
   private
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,50 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+    end
+
+
+
+    def create_address
+      @user = User.new(session["devise.regist_data"]["user"])
+      @address = Address.new(address_params)
+      unless @address.valid?
+        flash.now[:alert] = @address.errors.full_messages
+        render :new_address and return
+      end
+      @user.build_address(@address.attributes)
+      @user.save
+      sign_in(:user, @user)
+      # redirect_to root_path
+    end
+
+ 
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
+
+  def address_params
+    params.require(:address).permit(:zip,:prefecture, :city, :block, :building, :phone_number,
+    :lastname, :firstname, :lastname_kana, :firstname_kana)
+  end
+
+end
