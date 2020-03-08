cb02d2b0f0ac318a4c948b13fb47737615460659
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-08 12:33:19 +0900

    add test users be_valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    it "is valid with a nickname, email, password, password_confirmation, lastname, firstname, lastname_kana, firstname_kana, birthday" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "is invalid without a firstname_kana" do
       user = build(:user, firstname_kana: "")
       user.valid?
@@ -49,6 +54,33 @@ describe User do
       user = build(:user, birthday: "")
       user.valid?
       expect(user.errors[:birthday]).to include("can't be blank")
+    end
+
+
+
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "is valid with a password that has more than 7 characters " do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a password that has less than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
     end
 
   end
