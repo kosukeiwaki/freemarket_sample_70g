e2a1d6e3dd171f5101f94fa914c4a1f323db3752
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-08 12:20:26 +0900

    add test is invalid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,55 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
-    it "is invalid without a nickname" do
+
+    it "is invalid without a firstname_kana" do
+      user = build(:user, firstname_kana: "")
+      user.valid?
+      expect(user.errors[:firstname_kana]).to include("can't be blank")
     end
+
+    it "is invalid without an lastname_kana" do
+      user = build(:user, lastname_kana: "")
+      user.valid?
+      expect(user.errors[:lastname_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is invalid without an lastname" do
+      user = build(:user, lastname: "")
+      user.valid?
+      expect(user.errors[:lastname]).to include("can't be blank")
+    end
+
+    it "is invalid without a firstname" do
+      user = build(:user, firstname: "")
+      user.valid?
+      expect(user.errors[:firstname]).to include("can't be blank")
+    end
+
+    it "is invalid without an lastname_kana" do
+      user = build(:user, lastname_kana: "")
+      user.valid?
+      expect(user.errors[:lastname_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a firstname_kana" do
+      user = build(:user, firstname_kana: "")
+      user.valid?
+      expect(user.errors[:firstname_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without an birthday" do
+      user = build(:user, birthday: "")
+      user.valid?
+      expect(user.errors[:birthday]).to include("can't be blank")
+    end
+
   end
 end
