2797dd4e306db7d6baf5568ddd5b6cbf95266946
kosukeiwaki <k544274@gmail.com>
2020-03-09 19:44:53 +0900

    update_card

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# PAYJP_PRIVATE_KEY = 'sk_test_f7fbcf1683090edbdfed5d6d'
+# PAYJP_KEY = 'pk_test_159e4ef18c6c443cb75d5ae1'
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   
   resources :items
   resources :users
