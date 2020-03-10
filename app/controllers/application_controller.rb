b9c1c52a5a67610b6b3c5aaf9828cb001377abb8
shi-miya <colon9anje9@icloud.com>
2020-03-10 17:05:29 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
 
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+       keys: [:nickname,:firstname,:lastname,:birthday,:lastname_kana,:firstname_kana])
   end
 
   private
