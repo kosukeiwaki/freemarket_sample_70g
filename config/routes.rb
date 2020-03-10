5aea9c8b255b85bc29d21f51daf57c264732b61d
shi-miya <colon9anje9@icloud.com>
2020-03-10 17:06:16 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'items#index'
-  resources :items 
+  root "items#index"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
 
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  
+  resources :items
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
