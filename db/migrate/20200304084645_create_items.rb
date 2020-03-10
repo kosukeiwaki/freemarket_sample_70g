49e056f9662ac97569e7932d92547d6fd18eb5b7
shi-miya <colon9anje9@icloud.com>
2020-03-10 17:07:17 +0900

    Update 20200304084645_create_items.rb

--- db/migrate/20200304084645_create_items.rb
+++ db/migrate/20200304084645_create_items.rb
@@ -5,10 +5,14 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer    :price, null: false
       t.string     :size, null: false
       t.string     :brand
+
+
       t.string     :status, null: false
       t.integer    :fee, null: false
       t.string     :region, null: false
       t.string     :shipping_date, null: false
+      # t.references :user, foreign_key: true      
+      # t.references :item_category, foreign_key: true
 
       t.timestamps
     end
