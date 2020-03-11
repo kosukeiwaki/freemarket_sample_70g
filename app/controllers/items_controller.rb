9f6f0a1aa1e6d41527daa33d198afc02193622b0
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-11 21:33:24 +0900

    fix items.controller.rb for conflict

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :set_message, only: [:show, :confirm]
+
   def index
     @items = Item.all
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
@@ -32,11 +36,26 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  private
-    def item_params
-      params.require(:item).permit(:name, :price, :size, :brand, :detail, :prefecture_id,
-                                   :status, :fee, :shipping_date, images_attributes: [:picture]).merge(user_id: current_user.id)
+  def confirm
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_ACCESS_KEY]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
+  end
   
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :size, :brand, :detail, :prefecture_id,
+                                 :status, :fee, :shipping_date, images_attributes: [:picture]).merge(user_id: current_user.id)
+  end
 
 end
