2442e080cda94e4b6436ab134c7be9ec3b9f43a3
kosukeiwaki <k544274@gmail.com>
2020-03-12 17:21:49 +0900

    Update 20200310011714_add_user_id_to_items.rb

--- db/migrate/20200310011714_add_user_id_to_items.rb
+++ db/migrate/20200310011714_add_user_id_to_items.rb
@@ -1,5 +1,5 @@
 class AddUserIdToItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :items, :user, foreign_key: true
+    # add_reference :items, :user, foreign_key: true
   end
 end
