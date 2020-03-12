88dd79a5597ec1ac49bae89f262a7706a90fdfd1
kosukeiwaki <k544274@gmail.com>
2020-03-12 21:00:59 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :set_message, only: [:show, :confirm]
+  before_action :set_message, only: [:show]
 
   def index
     @images = Image.all
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    # @category = Category.find(params[:id])
   end
 
   def new
@@ -36,17 +36,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-  end
-
-  def confirm
-    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-    if @card.blank?
-      redirect_to action: "new"
-    else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_ACCESS_KEY]
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
   end
   
   private
