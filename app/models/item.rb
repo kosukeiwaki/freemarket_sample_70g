db46be6dcc6baac9291be5b0e537f1b2ba5b3417
wacky0803 <60645753+wacky0803@users.noreply.github.com>
2020-03-12 10:26:07 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,6 @@
 class Item < ApplicationRecord
   has_many :images
   validates :name, :price, :size, :status, :fee, :region, :shipping_date, :stok, presence: true
-
-
-
-
-
-
   def when_item_sold_out(item)
     if item.stock == 1
       "売り切れです"
