57b9bdd6df2cd53d57d8ea7b6c420a3e58650fe2
kosukeiwaki <k544274@gmail.com>
2020-03-13 19:52:57 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,9 @@ ActiveRecord::Schema.define(version: 2020_03_13_032403) do
     t.datetime "updated_at", null: false
     t.text "detail"
     t.integer "prefecture_id"
+    t.bigint "buyer_id"
     t.bigint "saler_id"
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["saler_id"], name: "index_items_on_saler_id"
   end
 
