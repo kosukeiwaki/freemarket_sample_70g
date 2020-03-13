25293678b6dad49c2097835b5df8bd520bb1b370
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-13 12:54:34 +0900

    add devise module omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
-  has_many :sns_credentials
   has_many :items
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -17,7 +14,9 @@ class User < ApplicationRecord
   }
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+  has_many :sns_credentials
+
   has_one :address
 
   accepts_nested_attributes_for :address
