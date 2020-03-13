dd47053e77491c1cc0a041b81bd8e8bf688aa509
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-13 14:04:02 +0900

    add from_omniauth method in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,17 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :address
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
+
 end
