793bc8c06e60849a7de2402eb79ce0a180f5f4ee
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-13 12:49:03 +0900

    add sns_credential table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+  has_many :sns_credentials
   has_many :items
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_063936) do
+ActiveRecord::Schema.define(version: 2020_03_13_034645) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 2020_03_12_063936) do
     t.index ["item_id"], name: "index_items_categories_on_item_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -101,4 +110,5 @@ ActiveRecord::Schema.define(version: 2020_03_12_063936) do
   add_foreign_key "items", "users"
   add_foreign_key "items_categories", "categories"
   add_foreign_key "items_categories", "items"
+  add_foreign_key "sns_credentials", "users"
 end
