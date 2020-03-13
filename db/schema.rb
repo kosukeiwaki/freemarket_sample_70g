eed0346fceb61962dc20a541a144179c1134af24
wacky0803 <60645753+wacky0803@users.noreply.github.com>
2020-03-13 21:11:02 +0900

    fix-root

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,9 @@ ActiveRecord::Schema.define(version: 2020_03_13_032403) do
     t.datetime "updated_at", null: false
     t.text "detail"
     t.integer "prefecture_id"
+    t.bigint "buyer_id"
     t.bigint "saler_id"
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["saler_id"], name: "index_items_on_saler_id"
   end
 
@@ -84,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_032403) do
     t.string "nickname", null: false
     t.string "firstname", null: false
     t.string "lastname", null: false
-    t.date "birthday", null: false
+    t.string "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
