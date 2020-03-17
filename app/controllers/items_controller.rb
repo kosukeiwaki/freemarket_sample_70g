7faabe403b534d1d986755a65fac71a3542d17f3
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-17 17:20:08 +0900

    fix destroy action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
 
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :delete]
 
   def index
     @images = Image.all
@@ -43,8 +43,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
   
