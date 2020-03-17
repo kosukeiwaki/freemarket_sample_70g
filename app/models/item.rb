894eacadb374409e594cae718ee120f320e48316
junya-gera <l.l-liy-s.sotals-element-l@docomo.ne.jp>
2020-03-17 17:06:11 +0900

    add dependent destroy to item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
     validates :price,  numericality: { only_integer: true ,message: '整数の数字を入力してください'}
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :prefecture
-    has_many :images
+    has_many :images, dependent: :destroy
     accepts_nested_attributes_for :images, allow_destroy: true
 
 
