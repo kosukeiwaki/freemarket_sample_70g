ebd86958fdff93e122b68a29be29375f6194e4c8
kosukeiwaki <k544274@gmail.com>
2020-03-20 16:00:26 +0900

    fix

---
+++ db/migrate/20200320065406_remove_culmun_from_items.rb
@@ -0,0 +1,5 @@
+class RemoveCulmunFromItems < ActiveRecord::Migration[5.2]
+  def change
+    remove_reference :items, :saler
+  end
+end
---
+++ db/migrate/20200320065607_add_culmun_from_items.rb
@@ -0,0 +1,5 @@
+class AddCulmunFromItems < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :items, :saler, foreign_key: { to_table: :users }
+  end
+end
