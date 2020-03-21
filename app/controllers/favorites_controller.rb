3312982d8d24c4d644710db5cd69d458628aecb9
shi-miya <colon9anje9@icloud.com>
2020-03-21 12:21:14 +0900

    add favorite btn func

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+    # @item = Item.find(params[:item_id])
     @favorite = current_user.favorites.create(item_id: params[:item_id])
+    # if favorite.save
     redirect_back(fallback_location: root_path)
+    # else
+    # redirect_to new_item_path(params[:id]) 
   end
 
   def destroy
+    # @item = Item.find(params[:item_id])
     @favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
     @favorite.destroy
     redirect_back(fallback_location: root_path)
